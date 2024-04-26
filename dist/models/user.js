24744bb4b7b80b4c41b0e98e24b754aa212e2213
chirag7pandey <chirag7pandey@gmail.com>
2024-04-26 11:16:38 +0530

    user schema  MongoDb Connect

---
+++ dist/models/user.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import validator from "validator";
+const schema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: [true, "Please enter ID"],
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter Name"],
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already Exist"],
+        required: [true, "Please enter Email"],
+        validate: validator.default.isEmail,
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add Photo"],
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female"],
+        required: [true, "please enter gender"]
+    },
+    dob: {
+        type: Date,
+        required: [true, "please enter your date of birth"],
+    },
+}, {
+    timestamps: true,
+});
+schema.virtual("age").get(function () {
+    const today = new Date();
+    const dob = this.dob;
+    let age = today.getFullYear() - dob.getFullYear();
+    if (today.getMonth() < dob.getMonth() ||
+        today.getMonth() === dob.getMonth()
+            && today.getDate() < dob.getDate()) {
+        age--;
+    }
+    return age;
+});
+export const User = mongoose.model("User", schema);
---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export interface NewUserRequestBody{
+    name:string;
+    email:string;
+    photo:string;
+    gender:string;
+    _id:number;
+    dob:Date;
+
+}
